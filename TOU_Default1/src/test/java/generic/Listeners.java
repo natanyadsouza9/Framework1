9a7539c9da1eba97caf86c304d46ed219b4bb4bb
natanya <natanya@192.168.1.121>
2020-10-15 15:06:12 -0700

    related to running tests in parallel

---
+++ TOU_Default1/src/test/java/generic/Listeners.java
@@ -0,0 +1,51 @@
+package generic;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+
+
+public class Listeners extends BaseTest implements ITestListener {
+
+	
+	public void onFinish(ITestContext arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onStart(ITestContext arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestFailure(ITestResult result) {
+		// TODO Auto-generated method stub
+		extentTest.get().fail(result.getThrowable());
+	}
+
+	public void onTestSkipped(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestStart(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestSuccess(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		extentTest.get().log(Status.PASS, "Test Passed");
+
+	}
+
+}
---
+++ TOU_Default1/src/test/java/scripts/TC2_launchOrangeHRM.java
@@ -0,0 +1,34 @@
+package scripts;
+
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.Status;
+
+import generic.BaseTest;
+import generic.Excel;
+import pom.LoginPage;
+
+public class TC2_launchOrangeHRM extends BaseTest {
+
+	public WebDriver driver;
+
+	@Test
+	public void launchOrangeHRM() throws InterruptedException, EncryptedDocumentException, InvalidFormatException, IOException {
+		
+		driver=initializeDriver();
+
+		test = extent.createTest("launchOrangeHRM");
+		extentTest.set(test);
+		LoginPage l=new LoginPage(driver);
+		extentTest.get().log(Status.INFO, l.setUserName(Excel.readData("Username")));
+
+		extentTest.get().log(Status.INFO,l.setPassword(Excel.readData("Password")));
+		extentTest.get().log(Status.INFO,l.clickLoginButton());
+		extentTest.get().log(Status.INFO,l.hoverOnPIM());
+	}
+}
